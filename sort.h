44bce12a61993327cbc302ed9d142bdcb551646a
Asser Elshafey <elshafeyasser@yahoo.com>
2023-07-22 20:25:05 +0300

    adding prototypes

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef sort_h
 #define sort_h
 
+#include <stdlib.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -17,6 +18,10 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 
 #endif /* sort_h */
