bb4f04e855c9fdc28b8b0432b24dc93ab7327795
Asser Elshafey <elshafeyasser@yahoo.com>
2023-07-23 22:07:10 +0300

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,20 +6,20 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *node;
+    listint_t *node;
 
-	if (list == NULL || (*list)->next == NULL)
-		return;
-	node = (*list)->next;
-	while (node)
-	{
-		while ((node->prev) && (node->prev->n > node->n))
-		{
-			node = swap_node(node, list);
-			print_list(*list);
-		}
-		node = node->next;
-	}
+    if (list == NULL || (*list)->next == NULL)
+        return;
+    node = (*list)->next;
+    while (node)
+    {
+        while ((node->prev) && (node->prev->n > node->n))
+        {
+            node = swap_node(node, list);
+            print_list(*list);
+        }
+        node = node->next;
+    }
 }
 /**
  *swap_node - swap a node for his previous one
@@ -29,18 +29,18 @@ void insertion_sort_list(listint_t **list)
  */
 listint_t *swap_node(listint_t *node, listint_t **list)
 {
-	listint_t *back = node->prev, *current = node;
-	/*NULL, 19, 48, 9, 71, 13, NULL*/
+    listint_t *back = node->prev, *current = node;
+    /*NULL, 19, 48, 9, 71, 13, NULL*/
 
-	back->next = current->next;
-	if (current->next)
-		current->next->prev = back;
-	current->next = back;
-	current->prev = back->prev;
-	back->prev = current;
-	if (current->prev)
-		current->prev->next = current;
-	else
-		*list = current;
-	return (current);
+    back->next = current->next;
+    if (current->next)
+        current->next->prev = back;
+    current->next = back;
+    current->prev = back->prev;
+    back->prev = current;
+    if (current->prev)
+        current->prev->next = current;
+    else
+        *list = current;
+    return (current);
 }
