f8c01cb82827dada708f6d8eab8401d4e529254e
Loay Waleed <loaywaleed9@gmail.com>
2023-07-23 23:17:13 +0300

    Fixing number of prints

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,15 +9,19 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int tmp, min;
+	int tmp, min, swapped_flag;
 
 	for (i = 0; i < size; i++)
 	{
 		min = i;
+		swapped_flag = 0;
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min])
+			{
 				min = j;
+				swapped_flag = 1;
+			}
 		}
 		if (j != i)
 		{
@@ -25,7 +29,8 @@ void selection_sort(int *array, size_t size)
 			tmp = array[min];
 			array[min] = array[i];
 			array[i] = tmp;
-			print_array(array, size);
+			if (swapped_flag)
+				print_array(array, size);
 		}
 	}
 }
