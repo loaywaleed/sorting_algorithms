16c46db47352b3b6e1dbb564a2530b4077c3f806
Loay Waleed <loaywaleed9@gmail.com>
2023-07-23 22:31:35 +0300

    Betty fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,32 +7,32 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *current, *prev, *next;
+	listint_t *current, *prev, *next;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
 
-    current = (*list)->next;
-    while (current != NULL)
-    {
-        prev = current->prev;
-        next = current->next;
-        while (prev != NULL && current->n < prev->n)
-        {
-            prev->next = next;
-            if (next != NULL)
-                next->prev = prev;
-            current->prev = prev->prev;
-            current->next = prev;
-            if (prev->prev != NULL)
-                prev->prev->next = current;
-            else
-                *list = current;
-            prev->prev = current;
-            print_list(*list);
-            prev = current->prev;
-            next = current->next;
-        }
-        current = next;
-    }
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		prev = current->prev;
+		next = current->next;
+		while (prev != NULL && current->n < prev->n)
+		{
+			prev->next = next;
+			if (next != NULL)
+				next->prev = prev;
+			current->prev = prev->prev;
+			current->next = prev;
+			if (prev->prev != NULL)
+				prev->prev->next = current;
+			else
+				*list = current;
+			prev->prev = current;
+			print_list(*list);
+			prev = current->prev;
+			next = current->next;
+		}
+		current = next;
+	}
 }
