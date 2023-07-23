085008308e5ef95182346678c2ff6f77d2e1cd0e
Loay Waleed <loaywaleed9@gmail.com>
2023-07-23 23:03:48 +0300

    Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that sorts array using selection sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int i, min, j, tmp;
+
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (j != i)
+		{
+			/* swap */
+			tmp = array[min];
+			array[min] = array[i];
+			array[i] = tmp;
+			print_array(array, size);
+		}
+	}
+}
